79e6d8571a5a099d80b8e5183dedde58fc343560
sharathesunshine-ux <sharathesunshine@gmail.com>
2026-03-23 15:22:06 +0530

    initial commit

---
+++ Average.py
@@ -0,0 +1,9 @@
+pen1 = 10
+pen2 = 15
+pen3 = 20
+pen4 = 25
+pen5 = 30
+sum = pen1+pen2+pen3+pen4+pen5
+print("The Sum of all 5 Pens are-",sum)
+average = sum/5
+print("The Average of all 5 pens are -", average)
