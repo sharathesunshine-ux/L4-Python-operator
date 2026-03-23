c1b130f03196092c65d2bf925803ac74363d8a4a
sharathesunshine-ux <sharathesunshine@gmail.com>
2026-03-23 15:48:46 +0530

    initial commit

---
+++ percentage calculation.py
@@ -0,0 +1,9 @@
+print("Enter Marks Obtained in 4 Subjects : ")
+Maths = int(input("Maths:"))
+English = int(input("English:"))
+Science = int(input("Science:"))
+Social = int(input("Social:"))
+sum = Maths+English+Science+Social
+print("The Sum of Maths,English,scienceSocial are ", sum)
+percentage = (sum/400)*100
+print("Percentage of marks obtained are:",percentage)
